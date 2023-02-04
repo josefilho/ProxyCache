f6d892e0b6415267b67c7b971f3332c86ceb5b8a
Jose Carlos (Linux) <iamjose.filho@gmail.com>
2023-02-04 06:30:09 -0400

    feat(Page): added 3 new functions
    getSize, getURL, setProtocol
    
    setProtocol is used to change from HTTP/1.1 to HTTP/1.0 to easily the communication

--- src/Page/Page.cpp
+++ src/Page/Page.cpp
@@ -1,40 +1,28 @@
 #include "Page.h"
 
 Page::Page(const std::string &DATA) {
-    if(DATA.empty()){
+    if (DATA.empty()) {
         return;
     }
+
+    data = DATA;
+
     // Splitting the data into headers and body
-    std::string line;
-    std::stringstream ss(DATA);
-
-    // Getting the headers
-    std::vector<std::string> data;
-    boost::split(data, DATA, boost::is_any_of("\r\n\r\n"));
-
-    std::vector<std::string> _headers;
-    boost::split(_headers, data[0], boost::is_any_of("\r\n"));
-    for(auto &header : _headers){
-        headers.push_back(header);
+    std::vector<std::string> split_data = split(DATA, "\r\n\r\n");
+    if (split_data.size() == 2) {
+        headers = split(split_data[0], "\r\n");
+        body = split_data[1];
+    } else {
+        headers = split(split_data[0], "\r\n");
     }
 
-    body = data[1];
-
-    // Getting the host
-    for(auto &header : headers){
-        if(header.find("Host: ") != std::string::npos){
-            host = header.substr(6);
-        }
+    // Getting the host, method, path and protocol
+    std::vector<std::string> first_line = split(headers[0], " ");
+    if (first_line.size() == 3) {
+        method = first_line[0];
+        path = first_line[1];
+        protocol = first_line[2];
     }
-
-    // Getting the method, path and protocol
-    std::vector<std::string> _method_path_protocol;
-    boost::split(_method_path_protocol, headers[0], boost::is_any_of(" "));
-    method = _method_path_protocol[0];
-    path = _method_path_protocol[1];
-    protocol = _method_path_protocol[2];
-
-
 }
 
 std::string Page::getBody() const {
@@ -59,4 +47,60 @@ std::string Page::getPath() const {
 
 std::string Page::getProtocol() const {
     return protocol;
+}
+
+size_t Page::getSize() const {
+    return body.size() + headers.size();
+}
+
+std::string Page::getURL() const {
+    return host + path;
+}
+
+Page::operator std::string() const {
+    return data;
+}
+
+void Page::setProtocol(const std::string &HTTP_PROTOCOL) {
+    if (HTTP_PROTOCOL.empty()) {
+        return;
+    }
+    std::string proto;
+    if (HTTP_PROTOCOL == "HTTP/1.0") {
+        proto = "HTTP/1.0";
+    } else {
+        if (HTTP_PROTOCOL == "HTTP/1.1") {
+            proto = "HTTP/1.1";
+        } else {
+            if (HTTP_PROTOCOL == "HTTP/2.0") {
+                proto = "HTTP/2.0";
+            } else {
+                if (HTTP_PROTOCOL == "HTTP/3.0") {
+                    proto = "HTTP/3.0";
+                } else {
+                    proto = "HTTP/1.1";
+                }
+            }
+        }
+    }
+    headers[0] = method + " " + path + " " + proto;
+    protocol = proto;
+    data.replace(data.find(protocol), protocol.size(), proto);
+}
+
+std::vector<std::string> Page::split(const std::string &str, const std::string &delim) {
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos) {
+            pos = str.length();
+        }
+        std::string token = str.substr(prev, pos - prev);
+        if (!token.empty()) {
+            tokens.push_back(token);
+        }
+        prev = pos + delim.length();
+    } while (pos < str.length() && prev < str.length());
+    return tokens;
 }
--- src/Page/Page.h
+++ src/Page/Page.h
@@ -7,17 +7,18 @@
 
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
-#include <boost/algorithm/string.hpp>
 
 class Page {
 private:
+    std::string data;
     std::vector<std::string> headers;
     std::string body;
     std::string host;
     std::string method;
     std::string path;
     std::string protocol;
+
+    static std::vector<std::string> split(const std::string& str, const std::string& delim);
 
 public:
     explicit Page(const std::string& DATA);
@@ -28,6 +29,12 @@ public:
     [[nodiscard]] std::string getMethod() const;
     [[nodiscard]] std::string getPath() const;
     [[nodiscard]] std::string getProtocol() const;
+    [[nodiscard]] size_t getSize() const;
+    [[nodiscard]] std::string getURL() const;
+
+    void setProtocol(const std::string& HTTP_PROTOCOL);
+
+    explicit operator std::string() const;
 };
 
 
